d32afa13712ba521342bca95da60da3b36a54cff
allanwupi <allan.wu@outlook.com.au>
2026-05-04 19:46:17 +0800

    Changed layout template to shorten route function calls

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,19 @@
 from flask import render_template, request, url_for, redirect
 from app import app
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET']) # Replace this with a login page later
 @app.route('/calendar', methods=['GET'])
 def calendar():
-    return render_template('calendar-page.html', 
-                           calendar_active='active',
-                           todo_active='inactive',
-                           friends_active='inactive',
-                           import_active='inactive')
+    return render_template('calendar-page.html', calendar_active=True)
 
 @app.route('/todo', methods=['GET'])
 def to_do_list():
-    return render_template('todo-page.html',
-                           calendar_active='inactive',
-                           todo_active='active',
-                           friends_active='inactive',
-                           import_active='inactive')
+    return render_template('todo-page.html', todo_active=True)
 
 @app.route('/friends', methods=['GET'])
 def friends():
-    return render_template('friends-page.html',
-                           calendar_active='inactive',
-                           todo_active='inactive',
-                           friends_active='active',
-                           import_active='inactive')
+    return render_template('friends-page.html', friends_active=True)
 
 @app.route('/import', methods=['GET'])
 def imported_calendars():
-    return render_template('import-page.html',
-                           calendar_active='inactive',
-                           todo_active='inactive',
-                           friends_active='inactive',
-                           import_active='active')
+    return render_template('import-page.html', import_active=True)
